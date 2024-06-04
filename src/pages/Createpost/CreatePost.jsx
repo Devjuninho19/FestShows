9a08f84db1c6c101eb2e5b845ab72a41baa494a2
henriquepaggue <henrique@paggue.io>
2024-06-04 17:39:18 -0300

    Finalizado a tela de criar evento

--- src/pages/Createpost/CreatePost.jsx
+++ src/pages/Createpost/CreatePost.jsx
@@ -1,9 +1,67 @@
 import React from "react";
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../context/AuthContext";
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+
+  const [tags, setTags] = useState([]);
+  const [forError, setFormError] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div>
-      <h1>Criação do evento</h1>
+      <h2>Criar Evento</h2>
+      <p>Escreva Sobre o Evento</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Adicione o título"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira a imagem que representa o seu evento"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo de criação"
+            onChange={(e) => setBody(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+      </form>
+      <button className="btn">Criar</button>
     </div>
   );
 };
